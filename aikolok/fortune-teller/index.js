a1de789de5183d49676c14a872591c1068d62790
aikolok <skybloom5878@gmail.com>
2023-05-18 22:02:13 -0400

    fortune teller homwework done

--- aikolok/fortune-teller/index.js
+++ aikolok/fortune-teller/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const numberOfChildren = 4;
+const partnerName = "Aidin";
+const geoLocation = "Chicago";
+const jobTitle = "software engineer";
+
+console.log(
+  `You will be a ${jobTitle} in ${geoLocation}, and married to ${partnerName} with ${numberOfChildren} kids.`
+);
